3f72a0faf3d3082885097f54a6ab5a2da6de38c4
eisenburg <isenburg-eliana@ku.edu>
2024-05-19 06:52:24 -0500

    character escapement

--- equip.py
+++ equip.py
@@ -5,12 +5,12 @@ class Equip:
     def __init__(self, name: str, item_type: str, skills: list[tuple[str, str]], only_char: str = None):
         self.name = name
         self.item_type = item_type
-        self.skills: list[tuple[str, str]] = skills.copy()
+        self.skills: list[tuple[str, str]] = skills
         self.only_char = only_char
 
     @staticmethod
     def from_line(push_line: str):
-        name = re.findall(r"name:\"([^\"]+)\"", push_line)[0]
+        name = re.findall(r"name:\"([^\"]+)\"", push_line)[0].replace("\\", "")
         item_type = (
                 re.findall(r"wpnType:\"([^\"]+)\"", push_line) +
                 re.findall(r"amrType:\"([^\"]+)\"", push_line) +
@@ -18,9 +18,12 @@ class Equip:
         )[0]
         skills_str = re.findall(r"skills:\[((?:\[\"[^\"]+\",\"[^\"]+\"],?)*)]", push_line)[0]
         if skills_str:
-            skills = re.findall(r"\[\"([^\"]+)\",\"([^\"]+)\"],?", push_line)
+            skills = re.findall(r"\[\"([^\"]+)\",\"([^\"]+)\"],?", skills_str)
         else:
             skills = []
         onlies = re.findall(r"only_user:\"([^\"]+)\"", push_line)
         only_user = onlies[0] if len(onlies) > 0 else None
+        if only_user is not None:
+            if "\\" in only_user:
+                only_user = only_user.replace("\\", "")
         return Equip(name, item_type, skills, only_user)
